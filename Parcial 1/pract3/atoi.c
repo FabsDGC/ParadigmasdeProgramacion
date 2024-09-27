7924d67762f4f6c960926287807f1c2938219250
FabsDGC <fabs.02.glezch@gmail.com>
2024-09-26 19:47:12 -0600

    CAMBIOS
    CAMBIOS

---
+++ Parcial 1/pract3/atoi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//made by fabs, uriel and omar
+
+//protoripo de función
+int atoi(const char *string);
+//Función principal
+
+int main(){
+
+}
+//resto de funciones
